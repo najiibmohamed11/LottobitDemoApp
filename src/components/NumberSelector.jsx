975c4efc16399653e4f00b56dc5555528c3d3cb5
najiibmohamed11 <xaawomaxamuudkaahiye@gmail.com>
2024-06-12 13:18:53 +0300

    preventint to enter less then 7 digites

--- src/components/NumberSelector.jsx
+++ src/components/NumberSelector.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import PyramidGrid from './PyramidGrid';
 import { useNumbers } from './NumberProvider';
 // import { useNumbers } from '../NumberContext';
 
 const NumberSelector = () => {
-  const { setSortedSelectedNumbers,sortedSelectedNumbersLength } = useNumbers();
+  const { setSortedSelectedNumbers,sortedSelectedNumbersLength  } = useNumbers();
+  const [insuvicent,setInsuvicent] = useState(false)
   const navigate = useNavigate();
+  const handleClick=()=>{
+    if(sortedSelectedNumbersLength==7){
+      navigate('/voting')
+
+    }else{
+      setInsuvicent(true);
+
+    }
+
+  }
 
   return (
     <div className="screen" id="select-numbers">
       <h2>Select your 7 game numbers</h2>
+      {insuvicent&&<p className='more-number-error'>PLEAS SELECT MORE NUMBERS</p>}
       <span>{sortedSelectedNumbersLength}</span>
       <PyramidGrid onNumbersChange={setSortedSelectedNumbers} />  
-      <button className="button blue" onClick={() => navigate('/voting')}>Next</button>
+      <button className="button blue" onClick={handleClick}>Next</button>
     </div>
   );
 }
--- src/components/VotingScreen.jsx
+++ src/components/VotingScreen.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PyramidGrid from './PyramidGrid';
 import { useNumbers } from './NumberProvider';
 import { useNavigate } from 'react-router-dom';
@@ -6,13 +6,27 @@ import { useNavigate } from 'react-router-dom';
 
 const VotingScreen = () => {
   const { setSortedVotedNumbers,votedListLength } = useNumbers();
+  const [insuvicent,setInsuvicent] = useState(false)
+
+  const handleClick=()=>{
+    if(votedListLength==7){
+      navigate('/result')
+
+    }else{
+      setInsuvicent(true);
+
+    }
+
+  }
   const navigate=useNavigate()  
     return (
       <div className="screen" id="vote-numbers">
         <h2>Vote for Numbers</h2>
+        {insuvicent&&<p className='more-number-error'>PLEAS SELECT MORE NUMBERS</p>}
+
         <span>{votedListLength}</span>
         <PyramidGrid onNumbersChange={setSortedVotedNumbers} />
-        <button className="button green" onClick={() => navigate('/result')}>Mine My Vote</button>
+        <button className="button green" onClick={handleClick}>Mine My Vote</button>
       </div>
     );
   }
